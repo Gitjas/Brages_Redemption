1a091a5f1739c2305547f9681d0129421a841dd5
Gitjas <Gitjas@users.noreply.github.com>
2023-06-05 11:16:21 +0200

    Update joining_bg2.d

--- c#brage/dialogue/joining_bg2.d
+++ c#brage/dialogue/joining_bg2.d
@@ -22,8 +22,13 @@ CHAIN
 IF WEIGHT #-1
 ~InParty("C#Brage") See("C#Brage") !StateCheck("C#Brage",CD_STATE_NOTVALID)
 Global("C#BE_OisigFarewell","AR0901",0)~ THEN BHOISIG farewell_oisig
-~Brage, I see you found your way into <CHARNAME>'s group. Does this mean...?~ DO ~SetGlobal("C#BE_OisigFarewell","AR0901",1)~
-== C#BrageJ ~It means foremost that my times of services to the temples have come to a hold, High Watcher Oisig. <CHARNAME> is a dear friend and in need of my help, so I will oblidge, and gladly. I thank you for helping me in my times of need.~
+~Brage, I see you found your way into <CHARNAME>'s group.~ DO ~SetGlobal("C#BE_OisigFarewell","AR0901",1)~
+/* Brage already talked to Sir Lothtyran */
+== BHOISIG IF ~Global("C#BE_LothtyranFarewell","AR0901",1)~ THEN ~Sir Lothtyran already told me your times of services to the temples have come to a hold.~
+/* Brage did not talk to Sir Lothtyran */
+== BHOISIG IF ~Global("C#BE_LothtyranFarewell","AR0901",0)~ THEN ~Does this mean...?~ 
+== C#BrageJ IF ~Global("C#BE_LothtyranFarewell","AR0901",0)~ THEN ~It means foremost that my times of services to the temples have come to a hold, High Watcher Oisig.~
+== C#BrageJ ~<CHARNAME> is a dear friend and in need of my help, so I will oblige <PRO_HISHER> call for support, and gladly. I thank you for helping me in my times of need, High Watcher.~
 == BHOISIG ~The temples thank you in their gods' names for your services, son. You will always be welcome here.~
 == BHOISIG IF ~Global("NoHelpBeholder","GLOBAL",1) !Dead("UnseeingEye")~ THEN ~What does this mean for our plea for help, though, <CHARNAME>?~ 
 == BHOISIG IF ~Global("BeholderPlot","GLOBAL",1)~ THEN ~As to you, <CHARNAME>...~
@@ -33,6 +38,16 @@ IF ~Global("NoHelpBeholder","GLOBAL",1)
 !Dead("UnseeingEye")~ THEN + 23
 IF ~Global("BeholderPlot","GLOBAL",1)~ THEN + 24
 
+CHAIN
+IF WEIGHT #-1
+~InParty("C#Brage") See("C#Brage") !StateCheck("C#Brage",CD_STATE_NOTVALID)
+!InMyArea("OISIG")
+Global("C#BE_OisigFarewell","AR0901",0)
+Global("C#BE_LothtyranFarewell","AR0901",0)~ THEN HELMPR farewell_lothtyran
+#32296 /* ~Be ever watchful, <LADYLORD>. Do you require the services of Helm?~ */ DO ~SetGlobal("C#BE_LothtyranFarewell","AR0901",1)~
+== C#BrageJ ~Sir Lothtyran, you see me in the company of <CHARNAME>, a good friend and in need of my support. My time as an improver for the temples has come to an end. I thank the temples deeply for their support and chances they have provided for me.~
+== HELMPR ~I understand. I will inform High Watcher Oisig. Go with Helm's blessing, son.~
+EXIT
 
 
 
@@ -78,7 +93,8 @@ IF ~~ THEN C#Brage2 joinbg2_06
 ~Wonderful! I am ready to go right now.~ 
 == C#Brage2 IF ~!Dead("UnseeingEye")~ THEN ~I would feel oblidged to obey the temple's wishes and look into the Unseeing Eye cult, though. I hope you will spare the time and help save the poor souls that went with those tricksters. But if not... I know the Order of the Radiant Heart is also looking into it, so I will not insist if we have more important things to do.~
 == C#Brage2 IF ~Dead("UnseeingEye")~ THEN ~You already clinsed the city of the "Unseeing Eye" cult, as I see. This leaves me with no more obligations to the temples.~
-== C#Brage2 IF ~Exists("OISIG")~ THEN ~Please, take me to High Watcher Oisig, so I can tell him my farewell.~
+== C#Brage2 IF ~InMyArea("OISIG")~ THEN ~Please, take me to High Watcher Oisig, so I can tell him my farewell.~
+== C#Brage2 IF ~!InMyArea("OISIG")~ THEN ~High Watcher Oisig seems to be absent - I'd need to speak with Sir Lothtyran before we go, so I can tell my farewells.~
 END
 IF ~~ THEN DO ~SetGlobal("C#BE_SpawnBrage","GLOBAL",22) SetGlobal("C#BE_BrageTalksBG2","GLOBAL",1) RealSetGlobalTimer("C#BE_BrageDialogueTimer","GLOBAL",1600) JoinParty()~ EXIT
 
@@ -91,13 +107,24 @@ IF ~~ THEN C#Brage2 joinbg2_07
 END
 ++ ~Agreed. I would be grateful if you'd come along, Brage.~ + joinbg2_06
 + ~Global("C#IM_ImoenStays","GLOBAL",0)~ + ~I can't spare any gold until I have the fare I need to go after Imoen.~ + joinbg2_08
++ ~GlobalGT("C#IM_ImoenStays","GLOBAL",0)~ + ~I can't spare any gold until I have the fare I need to go after this mage Irenicus.~ + joinbg2_08_a
 ++ ~No, I don't have a use for you yet.~ + joinbg2_04
 
 
 APPEND C#Brage2
 
 IF ~~ THEN joinbg2_08
-SAY ~Of course! I will not stand in the way of saving our dear friend. I have some savings... I will manage. So - I would join with you now?~
+SAY ~Of course! I will not stand in the way of saving our dear friend.~
+IF ~~ THEN + joinbg2_08_b
+END
+
+IF ~~ THEN joinbg2_08_a
+SAY ~Of course! I will not stand in the way of your urgent quest goals.~
+IF ~~ THEN + joinbg2_08_b
+END
+
+IF ~~ THEN joinbg2_08_b
+SAY ~I have some savings... I will manage until then. So - I would join with you now?~
 ++ ~Agreed. I would be grateful if you'd come along, Brage.~ + joinbg2_06
 ++ ~No, I don't have a use for you yet.~ + joinbg2_04
 END
